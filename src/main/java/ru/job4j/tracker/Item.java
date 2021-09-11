c860cca86858d7f28e37da543611017f225bbd53
mihaylinsergey <semi999@mail.ru>
2021-09-11 17:54:05 +0300

    2. Перегрузка конструктора [#173890 #201807]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -16,4 +16,19 @@ public class Item {
         this.id = id;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
