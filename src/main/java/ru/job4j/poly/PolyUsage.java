b9ce854462e629f53c5c5d157a1cff01e0faaa73
mihaylinsergey <semi999@mail.ru>
2021-09-16 21:16:51 +0300

    3.2. Зачем нужно приведение типов [#310019]

---
+++ src/main/java/ru/job4j/poly/PolyUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.poly;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        Vehicle plane = new Plane();
+        Vehicle traine = new Traine();
+        Vehicle bas = new Bas();
+        Vehicle[] vehicles = new Vehicle[] {plane, traine, bas};
+        for(Vehicle vehicle : vehicles){
+            vehicle.move();
+        }
+    }
+}
