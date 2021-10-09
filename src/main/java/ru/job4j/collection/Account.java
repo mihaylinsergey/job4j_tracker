6319ff414ce1b84d98f990fe8f801e50e4c2b5f4
mihaylinsergey <semi999@mail.ru>
2021-10-09 19:10:45 +0300

    3. Модели данных и HashSet. [#218530]

---
+++ src/main/java/ru/job4j/collection/Account.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Account {
+    private String passport;
+    private String username;
+    private String deposit;
+
+    public Account(String passport, String username, String deposit) {
+        this.deposit = deposit;
+        this.username = username;
+        this.passport = passport;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(passport, account.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "passport='" + passport + '\'' +
+                ", username='" + username + '\'' +
+                ", deposit='" + deposit + '\'' +
+                '}';
+    }
+}
--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import java.util.HashSet;
 
 public class UniqueText {
-
     public static boolean isEquals(String originText, String duplicateText) {
         boolean rsl = true;
         String[] origin = originText.split(" ");
