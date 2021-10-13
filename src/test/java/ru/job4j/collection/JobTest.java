f5744780fb6866f529bbcd9cc59fe9ea5c81e4cd
mihaylinsergey <semi999@mail.ru>
2021-10-13 22:45:46 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.collection;
+
+
+import org.junit.Test;
+import java.util.Comparator;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void JobDescByNameInc() {
+        Comparator<Job> jobDescByNameInc = new JobDescByNameInc();
+        int rsl = jobDescByNameInc.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void JobDescByNameDec() {
+        Comparator<Job> jobDescByNameDec = new JobDescByNameDec();
+        int rsl = jobDescByNameDec.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void JobDescByPriorityInc() {
+        Comparator<Job> jobDescByPriorityInc = new JobDescByPriorityInc();
+        int rsl = jobDescByPriorityInc.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void JobDescByPriorityDec() {
+        Comparator<Job> jobDescByPriorityDec = new JobDescByPriorityDec();
+        int rsl = jobDescByPriorityDec.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByNameInc().thenComparing(new JobDescByPriorityInc());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
