beb0e5422777586c2a5ce2e76f8c93d3cbb79b5a
mihaylinsergey <semi999@mail.ru>
2021-10-25 19:57:26 +0300

    2.2. Лямбда блок [#249208 #219104]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -7,7 +7,8 @@ public class LambdaUsage {
     public static void main(String[] args) {
         Comparator<Attachment> comparator = (left, right) -> {
             System.out.println("compare" + right.getSize() + ":" + left.getSize());
-            return right.getSize() - left.getSize();
+            return Integer.compare(right.getSize(), left.getSize());
         };
     }
 }
+
